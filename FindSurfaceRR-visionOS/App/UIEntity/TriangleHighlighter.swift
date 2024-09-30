97291c017bf61739efec76da67cc1630a68a1238
sgkim <sg.kim@curvsurf.com>
2024-09-30 18:28:58 +0900

    Upgrade to VisionOS 2.0 to fix performance issue: - introduce LowLevelMesh to improve performance during mesh updates for triangle highlighter.

--- FindSurfaceRR-visionOS/App/UIEntity/TriangleHighlighter.swift
+++ FindSurfaceRR-visionOS/App/UIEntity/TriangleHighlighter.swift
@@ -10,14 +10,44 @@ import RealityKit
 @MainActor
 final class TriangleHighlighter: Entity {
     
+    private let lowLevelMesh: LowLevelMesh
     private let model: ModelEntity
     
     required init() {
-        let submesh = Submesh(positions: [.zero, .zero, .zero], triangleIndices: [0, 1, 2])
-        let mesh = try! MeshResource.generate(name: "triangleMesh", from: submesh)
-        let material = SimpleMaterial(color: .green.withAlphaComponent(0.3), roughness: 0.75, isMetallic: false)
-        let model = ModelEntity(mesh: mesh, materials: [material])
         
+        let attributes: [LowLevelMesh.Attribute] = [
+            .init(semantic: .position, format: .float3, offset: 0),
+            .init(semantic: .normal, format: .float3, offset: MemoryLayout<simd_float3>.stride)
+        ]
+        
+        let layouts: [LowLevelMesh.Layout] = [
+            .init(bufferIndex: 0, bufferOffset: 0, bufferStride: MemoryLayout<simd_float3>.stride * 2)
+        ]
+        
+        let meshDescriptor = LowLevelMesh.Descriptor(vertexCapacity: 6,
+                                                     vertexAttributes: attributes,
+                                                     vertexLayouts: layouts,
+                                                     indexCapacity: 6,
+                                                     indexType: .uint32)
+        
+        let lowLevelMesh = try! LowLevelMesh(descriptor: meshDescriptor)
+        
+        lowLevelMesh.withUnsafeMutableIndices { rawIndices in
+            let indices = rawIndices.bindMemory(to: UInt32.self)
+            indices[0] = 0
+            indices[1] = 1
+            indices[2] = 2
+            indices[3] = 3
+            indices[4] = 4
+            indices[5] = 5
+        }
+        
+        let mesh = try! MeshResource(from: lowLevelMesh)
+        let material = UnlitMaterial(color: .green)
+        let model = ModelEntity(mesh: mesh, materials: [material])
+        model.components.set(OpacityComponent(opacity: 0.3))
+        
+        self.lowLevelMesh = lowLevelMesh
         self.model = model
         super.init()
         addChild(model)
@@ -26,7 +56,32 @@ final class TriangleHighlighter: Entity {
     func updateTriangle(_ point0: simd_float3,
                         _ point1: simd_float3,
                         _ point2: simd_float3) {
-        let submesh = Submesh(positions: [point0, point1, point2, point0, point2, point1], triangleIndices: [0, 1, 2, 3, 4, 5])
-        model.model?.mesh = try! .generate(name: "triangleMesh", from: submesh)
+        lowLevelMesh.withUnsafeMutableBytes(bufferIndex: 0) { rawVertex in
+            let vertices = rawVertex.bindMemory(to: simd_float3.self)
+            let normal = normalize(cross(normalize(point1 - point0), normalize(point2 - point0)))
+            vertices[0] = point0
+            vertices[1] = normal
+            vertices[2] = point1
+            vertices[3] = normal
+            vertices[4] = point2
+            vertices[5] = normal
+            vertices[6] = point0
+            vertices[7] = -normal
+            vertices[8] = point2
+            vertices[9] = -normal
+            vertices[10] = point1
+            vertices[11] = -normal
+        }
+        
+        var bounds = BoundingBox()
+        bounds.formUnion(point0)
+        bounds.formUnion(point1)
+        bounds.formUnion(point2)
+        
+        lowLevelMesh.parts.replaceAll([
+            LowLevelMesh.Part(indexCount: 3,
+                              topology: .triangle,
+                              bounds: bounds)
+        ])
     }
 }
